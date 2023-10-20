c9dd0deb75f4905ff4077e60116ec8cdaeb47c34
Rodolfo Silva <100287751+Jrodolfosilva@users.noreply.github.com>
2023-10-20 11:59:10 -0300

    feat: db and api

--- index.js
+++ index.js
@@ -1,43 +1,32 @@
 const  puppeteer = require("puppeteer");
-
-
-function checkJobs (){
-
-setInterval(()=>{
-
-    connectToWorkana()
-},10000)
-}
-
-checkJobs()
-
+const api = require('./api')
 async function connectToWorkana (){
 
 const browser = await puppeteer.launch({
-    headless:"new",
+    headless:"false",
     defaultViewport: null
 })
 
-const page = await browser.newPage()
+
+const page = await browser.newPage()//abro uma nova página 
+
+await page.goto('https://www.workana.com/jobs?category=it-programming&has_few_bids=1&language=pt&publication=1d') //navego até a url
+
+page.waitForSelector('.project-title') // Espero o selector está na DOM
+
+const dataNewJob = await page.$$eval('.project-title',(dados)=>{
+
+return dados.map((job)=>{
+    const title = job.innerText;
+    const link = job.children[0].href;
+    const datajob =  {title,link} 
+    return datajob
+})
 
 
-
-await page.goto('https://www.workana.com/jobs?category=it-programming&language=pt')
-
-
-const dataPage = await page.evaluate(()=>{
-
-    const title = document.querySelectorAll(".h3.project-title")
-
-   return title
 
 })
 
-console.log(dataPage)
+api.saveNewJob(dataNewJob)
+}connectToWorkana()
 
-await browser.close()
-
-}
-
-
-connectToWorkana()
