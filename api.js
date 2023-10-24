2129878286736a085654b879772bf5742de96955
Rodolfo Silva <100287751+Jrodolfosilva@users.noreply.github.com>
2023-10-24 00:31:34 -0300

    fix: performace

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = "./jobs.json";
-
+require('dotenv').config();
+const key = process.env.WAInstance
 let data = readFile()
 
 function readFile() {
@@ -37,7 +38,7 @@ module.exports = {
                 data.unshift(job)
                 fs.writeFileSync(path, JSON.stringify(data, null, 2), 'utf8');
 
-                const url = "http://18.230.140.98:3333/message/text?key=123"
+                const url = `http://18.230.140.98:3333/message/text?key=${key}`
                 const message ={
                     id:"558198857479",
                     message: `🧙${job.title} : ${job.link}`
--- app.js
+++ app.js
@@ -2,51 +2,37 @@ const puppeteer = require("puppeteer");
 const api = require('./api');
 const cron = require('node-cron');
 
-let browser; 
 
-async function getBrowserInstance() {
-    if (!browser) {
-        browser = await puppeteer.launch({
-            headless: 'new',
-            defaultViewport: null
-        });
-    }
-    return browser;
-}
+
+
 
 async function connectToWorkana() {
-    const browserInstance = await getBrowserInstance();
+  const  browser = await puppeteer.launch({
+        headless: 'new',
+        defaultViewport: null
+    });
 
-    try {
-        const pagePromises = []; // Array para armazenar promessas de páginas
-
+    try {       
         for (let i = 1; i < 4; i++) {
-            const pagePromise = (async () => {
-                const page = await browserInstance.newPage();
-                await page.goto(`https://www.workana.com/jobs?category=it-programming&has_few_bids=1&language=pt&publication=1d&page=${i}`);
-                await page.waitForSelector('.project-title');
-                
-                const dataNewJob = await page.$$eval('.project-title', (dados) => {
+            const page = await browser.newPage();
+            await page.goto(`https://www.workana.com/jobs?category=it-programming&has_few_bids=1&language=pt&publication=1d&page=${i}`,{timeout:60000});
+            await page.waitForSelector('.project-title');
+            const dataNewJob = await page.$$eval('.project-title', (dados) => {
                     return dados.map((job) => {
                         const title = job.innerText;
                         const link = job.children[0].href;
                         const postDate =  new Date().toISOString()
-
                        ; 
 
                         return { title, link,postDate };
                     });
                 });
 
-                await api.saveNewJob(dataNewJob);
                 await page.close(); // Feche a página após a conclusão
-            })();
-
-            pagePromises.push(pagePromise);
+                await api.saveNewJob(dataNewJob);
         }
 
-        // Espere que todas as páginas sejam processadas em paralelo
-        await Promise.all(pagePromises);
+      await browser.close()
     } catch (error) {
         console.error('Erro:', error);
     }
@@ -54,4 +40,4 @@ async function connectToWorkana() {
 
 connectToWorkana();
 
-cron.schedule('*/3 * * * * ', () => connectToWorkana());
+cron.schedule('*/2 * * * * ', () => connectToWorkana());
